8ec87445250a540c523619845808e85bbd1e85a1
mbudnikm <mbudnikm97@gmail.com>
2020-02-19 15:54:05 +0100

    finished symbols

--- exercises-training-jest/spec/symbols.spec.js
+++ exercises-training-jest/spec/symbols.spec.js
@@ -4,21 +4,21 @@ describe('Symbols', () => {
     var s1 = Symbol('secret');
     var s2 = Symbol('secret');
     var s3 = Symbol('secret');
-    expect(s1 == s2).toEqual( /* YOUR ANSWER HERE */ );
-    expect(s2 == s3).toEqual( /* YOUR ANSWER HERE */ );
-    expect(s1 == s3).toEqual( /* YOUR ANSWER HERE */ );
+    expect(s1 == s2).toEqual(false);
+    expect(s2 == s3).toEqual(false);
+    expect(s1 == s3).toEqual(false);
   });
 
   it('should be shared in global symbol registry', () => {
     var s1 = Symbol.for('secret');
     var s2 = Symbol.for('secret');
-    expect(s1 == s2).toEqual( /* YOUR ANSWER HERE */ );
+    expect(s1 == s2).toEqual(true);
   });
 
   it('should unique and shared symbols in separate registries', () => {
     var s1 = Symbol('secret');
     var s2 = Symbol.for('secret');
-    expect(s1 == s2).toEqual( /* YOUR ANSWER HERE */ );
+    expect(s1 == s2).toEqual(false);
   });
 
   it('should (appear/not appear) in Object.keys|values|entries', () => {
@@ -28,9 +28,9 @@ describe('Symbols', () => {
       [s1]: 'some',
       [s2]: 'secret',
     }
-    expect(Object.keys(secretObject).length).toEqual( /* YOUR ANSWER HERE */ );
-    expect(Object.values(secretObject).length).toEqual( /* YOUR ANSWER HERE */ );
-    expect(Object.entries(secretObject).length).toEqual( /* YOUR ANSWER HERE */ );
+    expect(Object.keys(secretObject).length).toEqual(0);
+    expect(Object.values(secretObject).length).toEqual(0);
+    expect(Object.entries(secretObject).length).toEqual(0);
   });
 
 });
