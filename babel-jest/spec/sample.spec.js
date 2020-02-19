c419ac19b59d0550b5c860996fc64533b56908b9
mbudnikm <mbudnikm97@gmail.com>
2020-02-19 12:18:03 +0100

    test with prvate has

--- babel-jest/spec/sample.spec.js
+++ babel-jest/spec/sample.spec.js
@@ -1,5 +1,22 @@
+class Human {
+    #dateOfBirth = "2020-01-01"
+
+    sayHi() {
+        return `I was born on ${this.#dateOfBirth}`
+    }
+}
+
 describe('Module...', () => {
     it('should ...', () => {
         expect(0).toBe(0)
     })
 })
+
+describe('Human', () => {
+    it('should display date of birth', () => {
+        const h = new Human
+        const msg = h.sayHi()
+        expect(msg).toEqual('I was born on 2020-01-01')
+    });
+})
+
