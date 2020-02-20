60f7b64318d29481f39b23178c79e9c3040e7853
mbudnikm <mbudnikm97@gmail.com>
2020-02-20 10:21:50 +0100

    finished descriptors

--- exercises-training-jest/spec/descriptors.spec.js
+++ exercises-training-jest/spec/descriptors.spec.js
@@ -4,14 +4,29 @@ describe('Descriptors', () => {
       firstName: 'John',
       lastName: 'Lennon',
       name(){ return `${this.firstName} ${this.lastName}` }
+      // get fullname(){ return `${this.firstName} ${this.lastName}`}
     }
+
+    Object.defineProperties(johnLiteral, {
+      fullname: {
+        enumerable: false,
+        // value: johnLiteral.name() <-- trzymamy rzecz dynamiczną, jak ją później zmienimy to będzie błąd
+        get (){ return this.name() }
+        // get (){ return `${this.firstName} ${this.lastName}`}
+      }
+    })
     
     // define a property on johnLiteral here
 
     expect(johnLiteral.fullname).toBe('John Lennon')
+    //expect(Object.keys(johnLiteral).length).toBe(3)
+    expect(Object.keys(johnLiteral)).toHaveLength(3)
+
+    johnLiteral.firstName = "Sir John"
+    expect(johnLiteral.fullname).toBe("Sir John Lennon")
   })
 
-  it('should create fully customizable properies on classes', () => {
+  it('should create fully customizable properties on classes', () => {
     class Person {
       constructor(firstName, lastName){
         this.firstName = firstName
@@ -21,10 +36,18 @@ describe('Descriptors', () => {
       name(){ return `${this.firstName} ${this.lastName}` }
     }
 
+    Object.defineProperties(Person.prototype, {
+      fullname: {
+        enumerable: false,
+        get (){ return this.name() }
+      }
+    })
+
     // define a property on Person class here
 
     var johnPerson = new Person('John', 'Lennon')
 
     expect(johnPerson.fullname).toBe('John Lennon')
+    expect(Object.keys(johnPerson)).toEqual(['firstName', 'lastName'])
   });
 });
