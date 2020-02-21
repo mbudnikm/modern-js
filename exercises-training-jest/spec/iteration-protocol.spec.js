7e88378159cb8b015a5887400a64090ff46d001e
mbudnikm <mbudnikm97@gmail.com>
2020-02-21 14:06:25 +0100

    finished iteration-protocol

--- exercises-training-jest/spec/iteration-protocol.spec.js
+++ exercises-training-jest/spec/iteration-protocol.spec.js
@@ -18,20 +18,27 @@ ITERATION PROTOCOL
   - Array.from
   - yield*
 */
+
+  function* iterateObject(obj) {
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        yield key
+      }
+    }
+  }
+
   it('should return iterator', () => {
     const array = ['a', 'b', 'c']
-    expect(array.entries() instanceof Array).toBe(/* YOUR ANSWER HERE */)
+    expect(array.entries() instanceof Array).toBe(false)
 
     const set = new Set(['a', 'b', 'c'])
-    expect(set.entries() instanceof Array).toBe(/* YOUR ANSWER HERE */)
+    expect(set.entries() instanceof Array).toBe(false)
 
     //...
   })
   
   it('should iterate over object keys', () => {
     // define iterateObject generator
-    function iterateObject() {
-		}
 
     const employee = getEmployee()
     const keys = ["id", "nationality", "keycardId", "account", "salary", "office", "departmentId", "firstName", "lastName", "title", "contractType", "email", "hiredAt", "expiresAt", "personalInfo", "skills", "bio", "imgURL"]
@@ -48,6 +55,29 @@ ITERATION PROTOCOL
 
   it('should make an object iterable', () => {
     const iterableEmployee = getEmployee()
+    // I solution
+    iterableEmployee[Symbol.iterator] = function* iterateObject() {
+      for (var key in this) {
+          yield key
+      }
+    }
+
+    // II solution
+    /* iterableEmployee[Symbol.iterator] = function(){
+      return iterateObject(this)
+    } */
+
+    // III solution
+    /* let iterableEmployee = {
+      ...getEmployee(),
+      [Symbol.iterator]() { return iterateObject(this)}
+    } */
+
+    //IV solution
+    /* iterableEmployee[Symbol.iterator] = function* (){
+      yield* iterateObject(this)
+    } */
+
     const keys = ["id", "nationality", "keycardId", "account", "salary", "office", "departmentId", "firstName", "lastName", "title", "contractType", "email", "hiredAt", "expiresAt", "personalInfo", "skills", "bio", "imgURL"]
 
     expect([...iterableEmployee]).toEqual(keys)
@@ -61,6 +91,26 @@ ITERATION PROTOCOL
       
       // make dreams come true!
       // figure it out!
+
+      /*Number.prototype[Symbol.iterator] = function* () {
+        const sign = Math.sign(this)
+        for (var value = 0; value != this; value += sign) {
+          yield value
+        }
+        yield value
+      }*/
+
+      Number.prototype[Symbol.iterator] = function* () {
+        if (this > 0) {
+          for (var value = 0; value <= this; value++) {
+            yield value
+          }
+        } else {
+          for (var value = 0; value >= this; value--) {
+            yield value
+          }
+        }
+      }
 
       expect([...10]).toEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
       expect([...(-10)]).toEqual([0, -1, -2, -3, -4, -5, -6, -7, -8, -9, -10])
